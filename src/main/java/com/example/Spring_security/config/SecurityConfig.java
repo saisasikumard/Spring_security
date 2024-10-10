f287a0902b768c926eb56739964a6e5fa976ee1c
saisasikumard <128885382+saisasikumard@users.noreply.github.com>
2024-10-10 23:51:55 +0530

    added role based security to Apis

--- src/main/java/com/example/Spring_security/config/SecurityConfig.java
+++ src/main/java/com/example/Spring_security/config/SecurityConfig.java
@@ -6,6 +6,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
@@ -15,18 +20,43 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public BCryptPasswordEncoder getPasswordEncoder(){
+        return new BCryptPasswordEncoder();
 
-        http
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/public/**").permitAll()  // Allow public access to these URLs
+    }
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails student= User.builder()
+                                .username("Sai")
+                                .password(getPasswordEncoder().encode("Sai@123"))
+                                .roles("STUDENT")
+                                .build();
+        UserDetails admin= User.builder()
+                .username("Sasi")
+                .password(getPasswordEncoder().encode("Sasi@123"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(student,admin);
+
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+                http
+                .authorizeHttpRequests
+                (       authorize -> authorize
+                        .requestMatchers("/public/**").permitAll()
+                        .requestMatchers("/student/**")
+                        .hasRole("STUDENT")
+                        .requestMatchers("/admin/**")
+                        .hasRole("ADMIN")
                         .anyRequest().authenticated()              // All other requests need authentication
                 )
-                .csrf(csrf -> csrf
-                        .ignoringRequestMatchers("/public/**")     // Disable CSRF protection for public endpoints only
+                .csrf
+                (
+                        csrf -> csrf
+                        .ignoringRequestMatchers("/public/**")
                 )
                 .formLogin(Customizer.withDefaults());  // Default form login
-
         return http.build();
     }
 }
